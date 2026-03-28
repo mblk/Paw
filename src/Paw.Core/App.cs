e80c6762daed554e99d68254fb85f3fdeab787d4
mblk <marcelblock@gmail.com>
2026-03-28 13:24:44 +0100

    Create bootstrapping class 'App'

---
+++ src/Paw.Core/App.cs
@@ -0,0 +1,118 @@
+﻿using Paw.Core.Engine;
+using Paw.Core.Graphics;
+using Paw.Core.Platforms;
+using Paw.Core.Utils;
+using System.Diagnostics;
+
+namespace Paw.Core;
+
+public abstract class App
+{
+    private readonly PlatformOptions _platformOptions;
+    private readonly WindowOptions _windowOptions;
+
+    public App(PlatformOptions platformOptions, WindowOptions windowOptions)
+    {
+        _platformOptions = platformOptions;
+        _windowOptions = windowOptions;
+    }
+
+    protected abstract void RegisterScenes(SceneManager sceneManager);
+
+    public void Run()
+    {
+        SystemInfo.PrintSystemAndBuildInfo();
+
+        // init platform
+
+        using var platform = PlatformFactory.CreatePlatform(_platformOptions);
+        using var window = platform.CreateWindow(_windowOptions);
+
+        var gl = window.GL;
+
+        // init assetmanager
+
+        var assetBaseDir = AssetManager.FindBaseDirectory();
+
+        var useAssetHotReloading = true;
+
+        AssetManager assetManager = useAssetHotReloading
+            ? new AssetManagerWithHotReload(assetBaseDir, gl)
+            : new AssetManager(assetBaseDir, gl);
+
+        // init scenes
+
+        using SceneManager sceneManager = new SceneManager(new SceneContext()
+        {
+            AssetManager = assetManager,
+        });
+
+        RegisterScenes(sceneManager);
+
+        // main loop
+
+        var sw = Stopwatch.StartNew();
+        var frameCount = 0;
+
+        var lastTime = DateTime.Now;
+
+        while (window.ProcessEvents() && !sceneManager.ExitRequested)
+        {
+            //
+            // update
+            //
+
+            var now = DateTime.Now;
+            float dt = (float)(now - lastTime).TotalSeconds;
+            lastTime = now;
+
+            (assetManager as AssetManagerWithHotReload)?.ProcessChanges();
+
+            sceneManager.Update(new UpdateContext()
+            {
+                DeltaTime = dt,
+                WindowSize = window.Size,
+                Input = window.Input,
+                SceneController = sceneManager,
+            });
+
+            //
+            // render
+            //
+
+            var (windowWidth, windowHeight) = window.Size;
+
+            gl.Viewport(0, 0, windowWidth, windowHeight);
+
+            gl.ClearColor(0f, 0f, 0f, 1f);
+            gl.Clear(GL.ClearBufferMask.COLOR_BUFFER_BIT);
+
+            sceneManager.Render(new RenderContext()
+            {
+                DeltaTime = dt,
+                WindowSize = window.Size,
+            });
+
+            window.SwapBuffers();
+
+            //
+            //
+            //
+
+            frameCount++;
+            if (sw.ElapsedMilliseconds >= 2500)
+            {
+                double fps = frameCount / sw.Elapsed.TotalSeconds;
+                Console.WriteLine($"FPS: {fps:F2}");
+                sw.Restart();
+                frameCount = 0;
+            }
+        }
+
+        // cleanup
+
+        Console.WriteLine("after mainloop");
+
+        Environment.Exit(0);
+    }
+}
